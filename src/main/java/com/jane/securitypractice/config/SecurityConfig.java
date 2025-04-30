27195c3b9a7133b926e18f025a11a5eb89916180
Jane Lee <janelee1721@gmail.com>
2025-04-30 11:15:30 +0900

    ✨Feat: 로그아웃 추가

--- src/main/java/com/jane/securitypractice/config/SecurityConfig.java
+++ src/main/java/com/jane/securitypractice/config/SecurityConfig.java
@@ -19,7 +19,11 @@ public class SecurityConfig {
                     .permitAll()
             )
             .logout(logout -> logout
-                    .logoutSuccessUrl("/login?logout=true")
+                    .logoutUrl("/logout") // 기본값
+                    .logoutSuccessUrl("/login?logout=true") // 로그아웃 후 리디렉션할 주소
+                    .invalidateHttpSession(true) // 세션 무효화
+                    .clearAuthentication(true) // 인증 정보 삭제
+                    .deleteCookies("JSESSIONID") // 세션 쿠키 제거
             )
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/login", "/register").permitAll()
